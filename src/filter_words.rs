50b0a39d7069725cfff866b60ec2e3d85bfc887d
Alec Bills <abills@andrew.cmu.edu>
2025-01-25 20:08:03 -0800

    add some enums

--- src/filter_words.rs
+++ src/filter_words.rs
@@ -1,41 +1,53 @@
+use std::str::Split;
+
+enum FilterType {
+    Equals(char),
+    Excludes(char),
+    Includes(char),
+}
+
+fn parse_filter_type(filter_type: char, letter: char) -> FilterType {
+    match filter_type {
+        '=' => FilterType::Equals(letter),
+        'x' => FilterType::Excludes(letter),
+        'i' => FilterType::Includes(letter),
+        _ => panic!("Invalid filter type {}. Use =, x, or i.", filter_type),
+    }
+}
+
 pub fn check_word(guess: &String, word: &String) -> bool {
-    let letter_guesses: Vec<String> = guess.split(" ").map(str::to_string).collect();
+    let letter_guesses: Split<&str> = guess.split(" ");
     let mut i = 0; // position
     for letter_guess in letter_guesses {
+        let filter_type = parse_filter_type(
+            letter_guess.chars().nth(0).unwrap(),
+            letter_guess.chars().nth(1).unwrap(),
+        );
         // equals
-        if letter_guess.chars().nth(0).unwrap() == '=' {
-            if letter_guess.chars().nth(1) != word.chars().nth(i) {
-                return false;
+        match filter_type {
+            FilterType::Equals(letter) => {
+                if letter != word.chars().nth(i).unwrap() {
+                    return false;
+                }
+            }
+            FilterType::Excludes(letter) => {
+                // What this actually means is that there are fewer occurances of the letter in the word than in the guess.
+                // count the occurances of the letter in the guess.
+                // Then count the occurances of the letter in the word.
+                let letter_count_in_guess: usize =
+                    guess.chars().filter(|c: &char| *c == letter).count();
+                let letter_count_in_word: usize =
+                    word.chars().filter(|c: &char| *c == letter).count();
+                if letter_count_in_guess <= letter_count_in_word {
+                    return false;
+                }
+            }
+            FilterType::Includes(letter) => {
+                if !word.contains(letter) {
+                    return false;
+                }
             }
         }
-
-        // excludes
-        if letter_guess.chars().nth(0).unwrap() == 'x' {
-            // What this actually means is that there are fewer occurances of the letter in the word than in the guess.
-            // count the occurances of the letter in the guess.
-            // Then count the occurances of the letter in the word.
-            let letter_count_in_guess: usize = guess
-                .chars()
-                .filter(|c: &char| *c == letter_guess.chars().nth(1).unwrap())
-                .count();
-            let letter_count_in_word: usize = word
-                .chars()
-                .filter(|c: &char| *c == letter_guess.chars().nth(1).unwrap())
-                .count();
-            if letter_count_in_guess <= letter_count_in_word {
-                return false;
-            }
-        }
-
-        // includes
-        if letter_guess.chars().nth(0).unwrap() == 'i' {
-            if !word.contains(letter_guess.chars().nth(1).expect("test")) {
-                return false;
-            } else if letter_guess.chars().nth(1) == word.chars().nth(i) {
-                return false;
-            }
-        }
-
         // increment the counter
         i += 1;
     }
